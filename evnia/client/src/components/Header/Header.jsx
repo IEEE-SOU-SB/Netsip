d367c5de98cccbb617a0e34b2ccf8c6f6a253848
Divyesh Patel <86349765+Divyesh230902@users.noreply.github.com>
2023-07-12 14:58:52 +0530

    header app

---
+++ evnia/client/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+// add header
+// from 'evnia/client/src/assets/images/Untitled.svg'
+
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import ModalVideo from 'react-modal-video';
+import logo from '../../assets/images/Untitled.svg';
+
+class Header extends Component {
+    
+}

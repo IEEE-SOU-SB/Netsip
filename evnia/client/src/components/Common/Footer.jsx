21a79d415a048ef9103b7fa1c4187f106f869266
Divyesh Patel <86349765+Divyesh230902@users.noreply.github.com>
2023-07-23 20:27:42 +0530

    footer change

--- evnia/client/src/components/Common/Footer.jsx
+++ evnia/client/src/components/Common/Footer.jsx
@@ -27,39 +27,39 @@ class Footer extends React.Component {
                                     <i className="icofont-phone"></i> Contact Our Authority
                                     <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Prof. Digant Parmar - <Link to="mailto:digantparmar@ieee.org">digantparmar@ieee.org</Link>
                                 </p>
-                                
+
                             </div>
                         </div>
 
-                        {/* <div className="col-lg-6 col-md-6">
+                        <div className="col-lg-6 col-md-6">
                             <div className="single-footer-widget">
                                 <h3>Social Connection</h3>
                                 <p>Don't miss Link thing! Receive daily news You should connect social area for Any Proper Updates Anytime.</p>
 
                                 <ul className="social-links">
                                     <li>
-                                        <Link to="https://www.facebook.com/" className="facebook" target="_blank">
+                                        <a href="https://www.facebook.com/IEEESilverOakUni?mibextid=LQQJ4d" className="facebook" target="_blank">
                                             <i className="icofont-facebook"></i>
-                                        </Link>
+                                        </a>
                                     </li>
                                     <li>
-                                        <Link to="https://twitter.com/" className="twitter" target="_blank">
+                                        <a href="https://twitter.com/IEEE_SilverOak?t=6kRosha9trv39oljXXI-AA&s=09" className="twitter" target="_blank">
                                             <i className="icofont-twitter"></i>
-                                        </Link>
+                                        </a>
                                     </li>
                                     <li>
-                                        <Link to="https://www.linkedin.com/" className="linkedin" target="_blank">
+                                        <a href="https://www.linkedin.com/company/ieee-silveroakuni/" className="linkedin" target="_blank">
                                             <i className="icofont-linkedin"></i>
-                                        </Link>
+                                        </a>
                                     </li>
                                     <li>
-                                        <Link to="https://www.instagram.com/" className="instagram" target="_blank">
+                                        <a href="https://instagram.com/ieee_silveroakuni?igshid=MzNlNGNkZWQ4Mg==" className="instagram" target="_blank">
                                             <i className="icofont-instagram"></i>
-                                        </Link>
+                                        </a>
                                     </li>
                                 </ul>
                             </div>
-                        </div> */}
+                        </div>
 
                         <div className="col-lg-12">
                             <div className="copyright-area">

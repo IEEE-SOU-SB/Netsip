ddbf6e15a55886ca66fd4c82525294f1f6c672c2
Nimit Desai <desainimit412@gmail.com>
2023-08-10 12:11:43 +0530

    bug repaired

--- evnia/client/src/components/Common/FunFact.jsx
+++ evnia/client/src/components/Common/FunFact.jsx
@@ -91,7 +91,7 @@ class FunFact extends React.Component {
                                             start={0}
                                             end={
                                                 this.state.didViewCountUp
-                                                    ? 200
+                                                    ? 202
                                                     : 0
                                             }
                                             duration={3}
